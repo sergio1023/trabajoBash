891632d5a8f2f0481dcf9c25e517f97878971465
sergio1023 <sergyfutbol37@gmail.com>
2022-05-03 19:02:35 +0200

    apartado borrar

--- administracionSergioMuñoz.sh
+++ administracionSergioMuñoz.sh
@@ -6,30 +6,38 @@
 function fpermisos(){
 	ruta=$(yad --width=400 --height=50 --title "Selecciona archivo" --form --center --file \
 	--column="") 2> /dev/null
-	permisos=$(yad --width=400 --height=50 --title "Establece permisos" --center --text-align=center \
-	--text="Establece permisos al fichero" --entry --button=Establecer:0 --button=Cancelar:1)
+	permisospropietarios=$(yad --width=400 --height=50 --title "Permisos para propietarios" --center --text-align=center \
+	--text="Establece permisos de propietario al fichero" --entry --button=Establecer:0 )
+	while [ $permisospropietarios -gt 7 ]
+		do
+			yad --width=400 --height=50 --center --text-align=center --column="" --text="Escriba un número entre el 0 y 7"
+			permisospropietarios=$(yad --width=400 --height=50 --title "Permisos para propietarios" --center --text-align=center \
+			--text="Establece permisos de propietario al fichero" --entry --button=Establecer:0 )
+		done
 	
-	ans=$?
-	
-		if [ $ans -eq 0 ]
-		then
-			chmod $permisos $ruta
-			yad --width=400 --height=50 --center --text-align=center --column="" --text="Permisos establecidos"
-		else
-			while [ $ans -eq 1 ]
-			do
-				yad --width=400 --height=50 --center --text-align=center --column="" --text="Ha habido un error al establecer permisos, pruebe otra vez"
-				ruta=$(yad --width=400 --height=50 --title "Selecciona archivo" --form --center --file \
-				--column="") 2> /dev/null
-				permisos=$(yad --width=400 --height=50 --title "Establece permisos" --center --text-align=center \
-				--text="Establece permisos al fichero" --entry --button=Establecer:0 --button=Cancelar:1)
-			done
-		fi
+	permisosgrupos=$(yad --width=400 --height=50 --title "Permisos para grupos" --center --text-align=center \
+	--text="Establece permisos de grupos al fichero" --entry --button=Establecer:0 )
+	while [ $permisosgrupos -gt 7 ]
+		do
+			yad --width=400 --height=50 --center --text-align=center --column="" --text="Escriba un número entre el 0 y 7"
+			permisosgrupos=$(yad --width=400 --height=50 --title "Permisos para grupos" --center --text-align=center \
+			--text="Establece permisos de grupos al fichero" --entry --button=Establecer:0 )
+		done
+	permisosotros=$(yad --width=400 --height=50 --title "Permisos para otros" --center --text-align=center \
+	--text="Establece permisos de otros al fichero" --entry --button=Establecer:0 )
+	while [ $permisosotros -gt 7 ]
+		do
+			yad --width=400 --height=50 --center --text-align=center --column="" --text="Escriba un número entre el 0 y 7"
+			permisosotros=$(yad --width=400 --height=50 --title "Permisos para otros" --center --text-align=center \
+			--text="Establece permisos de otros al fichero" --entry --button=Establecer:0 )
+		done
 		
+		permisos="$permisospropietarios$permisosgrupos$permisosotros"
+		chmod $permisos $ruta
 		comandols=$(ls -l $ruta)
 		echo $comandols
-	resultado=$(yad --width=400 --height=300 --title "Permisos finales del archivo" --center  \
-	--text="${comandols}")
+			resultado=$(yad --width=400 --height=300 --title "Permisos finales del archivo" --center  \
+			--text="${comandols}")
  }
 function ftareas(){
 	echo "Tarea"
@@ -37,7 +45,13 @@ function ftareas(){
 function fborrar(){
 	borrar=$(yad --width=300 --height=300 --title "¿Qué quieres borrar?" --form --center --file \
 	--column="" ) 2> /dev/null
-	mv $borrar /basura
+	mv $borrar /Escritorio/trabajoBash/basura
+	#type  /Escritorio/trabajoBash/basura/ruta.txt >> $borrar Almacenar en ruta.txt la ruta que vamos a borrar 
+	#nombreborrar = cutd -d "/ > ruta.txt" filtrar por el nombre de archivo
+	concatenaruta="ruta$nombreborrar"
+	#rename ruta.txt $concatenaruta concatenarlo para que quede un nombre adecuado
+	
+	
 	resultadoborrar=$(yad --width=500 --height=300 --title "Archivo borrado" --center --text-align=center --list  \
 	--column="" --text="Has borrado:" ${borrar})
 }
